39011b814026af073604a8f48ea3d00a03a6adf7
wuchuheng <root@wuchuheng.com>
2023-01-04 09:23:10 +0800

    Initial Commit

---
+++ wuchuheng/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wuchuheng
+//
+//  Created by wuchuheng on 2023/1/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wuchuheng/wuchuhengApp.swift
@@ -0,0 +1,17 @@
+//
+//  wuchuhengApp.swift
+//  wuchuheng
+//
+//  Created by wuchuheng on 2023/1/4.
+//
+
+import SwiftUI
+
+@main
+struct wuchuhengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
